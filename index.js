8c595892d3ebecdf0c3d69cb858a8cd21df2c917
Melissa Yamileth Cruz Pineda <meli_ukepless@ufg.edu.sv>
2024-10-17 07:27:58 -0600

    Se agrego el CORS al BackEnd

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require ("express");
+const cors = require("cors");
 const app = express();
 const puerto = 3000;
+app.use(cors());
 
 app.get("/usuario",function(req, res){
     res.json({
